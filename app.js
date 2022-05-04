244349ca5fbf79d3f2251e4c0d761230c14f14ec
Tommy McG <thomasjmcgovern98@gmail.com>
2022-05-04 18:12:47 -0400

    basics of SQL integrated into inquirer

--- app.js
+++ app.js
@@ -12,19 +12,51 @@ const db = mysql.createConnection(
 )
 
 const promptUser = () => {
-    inquirer.prompt({
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'action',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
-    })
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'action',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+        }
+    ])
     .then(({ action }) => {
+        console.log("Action: " + action);
+    
         switch (action) {
             case 'View all departments':
+                var sql = `SELECT * FROM department`;
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    }
+                    console.table(rows);
+                })
                 break;
             case 'View all roles':
+                var sql = `SELECT role.*, department.name
+                AS department_name 
+                FROM role
+                LEFT JOIN department
+                ON role.department_id = department.id`;
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    }
+                    console.table(rows);
+                })
                 break;
             case 'View all employees':
+                var sql = `SELECT * FROM employee`;
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    }
+                    console.table(rows);
+                })
                 break;
             case 'Add a department':
                 break;
@@ -34,8 +66,10 @@ const promptUser = () => {
                 break;
             case 'Update an employee role':
                 break;
-            case 'Exit':
-                break;
         }
-    })
+    });
 }
+
+promptUser().catch(err => {
+    console.log(err);
+});
