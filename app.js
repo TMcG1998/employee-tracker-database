93aaa9ea1c1c6903921f661b80d4f8cc8b59f5ca
Tommy McG <thomasjmcgovern98@gmail.com>
2022-05-04 15:56:04 -0400

    basic file structure and questions

---
+++ app.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    inquirer.prompt({
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'action',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+    })
+    .then(({ action }) => {
+        switch (action) {
+            case 'View all departments':
+                break;
+            case 'View all roles':
+                break;
+            case 'View all employees':
+                break;
+            case 'Add a department':
+                break;
+            case 'Add a role':
+                break;
+            case 'Add an employee':
+                break;
+            case 'Update an employee role':
+                break;
+            case 'Exit':
+                break;
+        }
+    })
+}
